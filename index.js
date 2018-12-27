29a8931ba64ec6cde307bf7fde3d17ecee8db161
Kishore Devaraj <kishoregrylls@gmail.com>
2018-12-27 12:25:44 +0530

    App created using create-react-native-app and added header component and styling to it

---
+++ index.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+
+const App = () => (
+    <Header />
+);
+
+AppRegistry.registerComponent('albums', () => App);
